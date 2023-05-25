d3d13e76a41fdd93326352cd390bae3d3977794c
SEAKER <SEAKER@DESKTOP-B344CKE>
2023-05-25 22:11:46 +0100

    Adicionar arquivos de projeto.

---
+++ Kudila Gestao/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("Kudila Gestão de Farmacias")]
+[assembly: AssemblyDescription("Kudila Gestão de Farmacias")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Kudila")]
+[assembly: AssemblyProduct("Kudila Gestão de Farmacias")]
+[assembly: AssemblyCopyright("Copyright © Kudila  2023")]
+[assembly: AssemblyTrademark("Kudila Gestão")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("697f35b7-7444-4d1b-b957-899de5442904")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
